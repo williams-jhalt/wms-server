36d45e1dab4aa9534630c37aa0f8ea274faf5a9f
johnh <johnh@DESKTOP-B7JMS42.hq.williamstradingco.com>
2017-03-23 13:21:10 -0400

    separated invoice export and invoice ftp upload

--- src/AppBundle/Command/ExportInvoicesCommand.php
+++ src/AppBundle/Command/ExportInvoicesCommand.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Command;
 
 use DateTime;
+use SplFileInfo;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -12,27 +13,31 @@ class ExportInvoicesCommand extends ContainerAwareCommand {
 
     protected function configure() {
         $this->setName('app:export_invoices')
-                ->setDescription('Export invoices to FTP')
+                ->setDescription('Export invoices to CSV')
                 ->addArgument('customerNumber', InputArgument::REQUIRED, "Customer Number")
-                ->addArgument('hostname', InputArgument::REQUIRED, "FTP Hostname")
-                ->addArgument('username', InputArgument::REQUIRED, "FTP Username")
-                ->addArgument('password', InputArgument::REQUIRED, "FTP Password");
+                ->addArgument('startDate', InputArgument::REQUIRED, "Start Date")
+                ->addArgument('endDate', InputArgument::REQUIRED, "End Date")
+                ->addArgument('headerFile', InputArgument::REQUIRED, "Header Filename")
+                ->addArgument('detailFile', InputArgument::REQUIRED, "Detail Filename");
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
         
-        $startDate = new DateTime("yesterday");
-        $endDate = new DateTime("tomorrow");
+        $startDate = new DateTime($input->getArgument('startDate'));
+        $endDate = new DateTime($input->getArgument('endDate'));
+        
+        $headerFile = new SplFileInfo($input->getArgument('headerFile'));
+        $detailFile = new SplFileInfo($input->getArgument('detailFile'));
 
         $service = $this->getContainer()->get('app.export_service');
         $output->write("Beginning invoice export...\n");
-        $service->uploadInvoicesToFtp(
+        $service->exportInvoiceData(
                 $input->getArgument('customerNumber'), 
                 $startDate, 
                 $endDate, 
-                $input->getArgument('hostname'),
-                $input->getArgument('username'),
-                $input->getArgument('password')
+                false,
+                $headerFile,
+                $detailFile
         );
 
         $output->write("Finished!\n\n");
---
+++ src/AppBundle/Command/UploadInvoicesCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\Command;
+
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class UploadInvoicesCommand extends ContainerAwareCommand {
+
+    protected function configure() {
+        $this->setName('app:upload_invoices')
+                ->setDescription('Export invoices to FTP')
+                ->addArgument('customerNumber', InputArgument::REQUIRED, "Customer Number")
+                ->addArgument('hostname', InputArgument::REQUIRED, "FTP Hostname")
+                ->addArgument('username', InputArgument::REQUIRED, "FTP Username")
+                ->addArgument('password', InputArgument::REQUIRED, "FTP Password");
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        
+        $startDate = new DateTime("yesterday");
+        $endDate = new DateTime("tomorrow");
+
+        $service = $this->getContainer()->get('app.export_service');
+        $output->write("Beginning invoice export...\n");
+        $service->uploadInvoicesToFtp(
+                $input->getArgument('customerNumber'), 
+                $startDate, 
+                $endDate, 
+                $input->getArgument('hostname'),
+                $input->getArgument('username'),
+                $input->getArgument('password')
+        );
+
+        $output->write("Finished!\n\n");
+    }
+
+}

de1394e8bc588c8f8a166009198a366157a09385
johnh <johnh@DESKTOP-B7JMS42.hq.williamstradingco.com>
2017-06-05 13:35:31 -0400

    made changes for michael

--- src/AppBundle/Command/UploadInvoicesCommand.php
+++ src/AppBundle/Command/UploadInvoicesCommand.php
@@ -16,23 +16,29 @@ class UploadInvoicesCommand extends ContainerAwareCommand {
                 ->addArgument('customerNumber', InputArgument::REQUIRED, "Customer Number")
                 ->addArgument('hostname', InputArgument::REQUIRED, "FTP Hostname")
                 ->addArgument('username', InputArgument::REQUIRED, "FTP Username")
-                ->addArgument('password', InputArgument::REQUIRED, "FTP Password");
+                ->addArgument('password', InputArgument::REQUIRED, "FTP Password")
+                ->addArgument('startDate', InputArgument::OPTIONAL, "Start Date")
+                ->addArgument('endDate', InputArgument::OPTIONAL, "End Date");
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
-        
-        $startDate = new DateTime("four weeks ago");
-        $endDate = new DateTime("tomorrow");
+
+        if ($input->hasArgument('startDate')) {
+            $startDate = new DateTime($input->getArgument('startDate'));
+        } else {
+            $startDate = new DateTime("yesterday");
+        }
+
+        if ($input->hasArgument('endDate')) {
+            $endDate = new DateTime($input->getArgument('endDate'));
+        } else {
+            $endDate = new DateTime("tomorrow");
+        }
 
         $service = $this->getContainer()->get('app.export_service');
         $output->write("Beginning invoice export...\n");
         $service->uploadInvoicesToFtp(
-                $input->getArgument('customerNumber'), 
-                $startDate, 
-                $endDate, 
-                $input->getArgument('hostname'),
-                $input->getArgument('username'),
-                $input->getArgument('password')
+                $input->getArgument('customerNumber'), $startDate, $endDate, $input->getArgument('hostname'), $input->getArgument('username'), $input->getArgument('password')
         );
 
         $output->write("Finished!\n\n");
